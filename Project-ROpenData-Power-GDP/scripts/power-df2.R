5cc5c9b36bc1bab07aadf0cd19bc1a86fe35f3d7
tingtingyeh <tingchi1992@gmail.com>
2017-02-26 01:39:34 +0800

    Update power-df2.R

--- Project-ROpenData-Power-GDP/scripts/power-df2.R
+++ Project-ROpenData-Power-GDP/scripts/power-df2.R
@@ -1,8 +1,8 @@
 #' 提示：
-#' 我會用到filter + grepl, 來挑出英文的id
-#'    ps. "^[A-Z]" 代表要尋找英文大寫字母開頭的字串
-#' mutate 來將年度從民國變成西元
-#' 最後用 mutate + gsub 把 id 中的"."給去除
+#' 會用到filter + grepl來挑出英文的id。
+#'    ps. "^[A-Z]" 代表要尋找以大寫英文字母開頭的字串。
+#' mutate 來將年度從民國變成西元。
+#' 最後用 mutate + gsub 將 id 中的"."給去除。
 
 power.df2 <- NULL # 請將 NULL 替換成你的程式碼
 
